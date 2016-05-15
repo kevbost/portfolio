da0d1dd718e4671e0a3b3fe0648fbf4c78ed9246
Kevin Bost <kevbost@gmail.com>
2016-05-15 19:27:59 -0400

    added pdf

--- build/export.js
+++ build/export.js
@@ -9,6 +9,7 @@ module.exports = {
     "jspm_packages/system-csp-production.js",
     "styles/main.css",
     "src/copy-text.json",
+    "src/V5-May-14-2016.pdf",
     "jspm_packages/npm/font-awesome@4.5.0/css/font-awesome.min.css",
     "jspm_packages/npm/font-awesome@4.5.0/fonts/*",
     "jspm_packages/github/github/fetch@0.11.0.js",
